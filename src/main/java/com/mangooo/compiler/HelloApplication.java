5c3acc33656eae99f596b7f6745850fccafbfed2
MohammedRagab00 <Mohammed_Ragab_@outlook.com>
2025-05-13 02:02:14 +0300

    added logo

--- src/main/java/com/mangooo/compiler/HelloApplication.java
+++ src/main/java/com/mangooo/compiler/HelloApplication.java
@@ -3,17 +3,20 @@ package com.mangooo.compiler;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
-        // Removed the explicit stylesheet addition here as it will be handled by the controller
-        stage.setTitle("Compiler");
+
+        stage.getIcons().add(new Image(Objects.requireNonNull(HelloApplication.class.getResourceAsStream("/icon.png"))));
+        stage.setTitle("Manga Compiler");
         stage.setScene(scene);
         stage.show();
     }
